4ec8737e603d2b45480565a137dd35bc3aefb8ef
Christopher Elias <celias@costar.co.uk>
2024-12-19 10:19:05 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import ListGroup from "./components/ListGroup";
+
+function App(){
+  let items = ["New York", "London", "Paris", "Tokyo", "Madrid"];
+  const handleSelectItem = (item:string) => {console.log(item)};
+  return <div><ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/></div>
+
+}
+
+export default App;
